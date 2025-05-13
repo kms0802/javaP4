c81970859a9b3e92821675dda422f339acc73262
kms0802 <kms0802@gmail.com>
2025-05-13 11:26:07 +0900

    05_13

--- 새 폴더/javaP4_Project/src/J0513.java
+++ 새 폴더/javaP4_Project/src/J0513.java
@@ -4,13 +4,13 @@ public class J0513 {
      public static void main(String[]args) {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
-        System.out.println("몇까지?");
+        System.out.println("숫자로 입력하세요.");
         int N = scanner.nextInt();
         for (int i = 1; i <= N; i++) {
             if (!(i % 2 == 0)) {
                 sum = sum + i;
             }
         }
-        System.out.println(sum);
+        System.out.println("홀수합 계산 완료 : [" + sum + "]");
     }
 }
